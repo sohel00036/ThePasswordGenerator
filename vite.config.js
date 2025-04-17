bc5cc4c30ab094700aa32ae8c581bca9da7cef1f
Sohel Hussain <sahilmansuri0036@gmail.com>
2025-04-18 05:20:02 +0530

    Updated vite.config.js with changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [tailwindcss(),react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
+  base: '/my-vite-app/', // ← yeh line add kar
+  plugins: [tailwindcss(), react()],
 })
